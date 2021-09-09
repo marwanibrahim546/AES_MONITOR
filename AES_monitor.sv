7d2fd4aedb44fbe62baa9464a8a3df5e139f56ad
marwan ibrahim zaki <50636605+marwanibrahim546@users.noreply.github.com>
2021-09-09 20:09:55 +0200

    edit the factory

---
+++ AES_monitor.sv
@@ -0,0 +1,23 @@
+class AES_monitor_before extends uvm_monitor;
+
+	`uvm_component_utils(AES_monitor_before)
+
+
+
+
+
+
+endclass : AES_monitor_before
+
+////////////////////////////////////////////////////////////////////
+
+class AES_monitor_after extends uvm_monitor;
+
+	`uvm_component_utils(AES_monitor_after)
+
+
+
+
+
+
+endclass : AES_monitor_after
