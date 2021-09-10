fd3f6292e4e88aa8d0490694b10e6101350a02ba
marwan ibrahim zaki <50636605+marwanibrahim546@users.noreply.github.com>
2021-09-10 23:16:36 +0200

    add the agent

---
+++ AES_config.sv
@@ -0,0 +1,7 @@
+class AES_configuration extends uvm_object;
+	`uvm_object_utils(simpleadder_configuration)
+
+	function new(string name = "");
+		super.new(name);
+	endfunction: new
+endclass: AES_configuration
